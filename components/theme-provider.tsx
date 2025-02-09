1c078773ff6964dc5e2836254de78f372ec2f7a1
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-02-09 17:00:15 -0600

    fix: shadcn dark mode hydration error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,16 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from 'react'
+const NextThemesProvider = dynamic(
+	() => import('next-themes').then((e) => e.ThemeProvider),
+	{
+		ssr: false,
+	}
+)
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+import { type ThemeProviderProps } from 'next-themes'
+import dynamic from 'next/dynamic'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
