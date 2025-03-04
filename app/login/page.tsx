d9aabd2f4a070d5fbdad81f7e87eb1d371527811
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-03-04 03:49:50 +0000

    style: remove logs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,6 @@ export default async function LoginPage() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    console.log(session);
     redirect('/dashboard');
   }
   // const res = await createUser({
