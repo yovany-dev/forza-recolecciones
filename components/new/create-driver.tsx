020842244fc3c8041466fa915a756cac81faee5d
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-03-05 00:49:16 +0000

    test(driver): alert messages

--- components/new/create-driver.tsx
+++ components/new/create-driver.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { CircleCheck } from "lucide-react";
+import { AlertCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,10 +13,27 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 import { driverSchema, driverSchemaType } from "@/lib/zod/driver";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+
+const alertMessage = (success: boolean, message: string) => {
+  return (
+    <Alert className="text-green-400 border-green-400">
+      {success ? (
+        <CircleCheck className="h-4 w-4" />
+      ) : (
+        <AlertCircle className="h-4 w-4" />
+      )}
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        Your session has expired. Please log in again.
+      </AlertDescription>
+    </Alert>
+  );
+};
 
 const CreateDriver = () => {
   const {
@@ -32,7 +51,11 @@ const CreateDriver = () => {
       body: JSON.stringify(data),
     });
     const driver = await res.json();
-    console.log(driver);
+    if (driver.status == 201) {
+      console.log("Piloto creado exitosamente.");
+    } else if (driver.status == 409) {
+      console.log(driver.errorMessage);
+    }
   };
   return (
     <Card>
@@ -50,10 +73,7 @@ const CreateDriver = () => {
         >
           <div className="space-y-1">
             <Label htmlFor="employeeNumber">No. Gafete</Label>
-            <Input
-              id="employeeNumber"
-              {...register("employeeNumber")}
-            />
+            <Input id="employeeNumber" {...register("employeeNumber")} />
             <p className="text-xs text-red-500">
               {errors.employeeNumber?.message}
             </p>
@@ -86,6 +106,15 @@ const CreateDriver = () => {
               defaultValue="Piloto Recolector"
               disabled
             />
+          </div>
+          <div className="col-span-2">
+            {/* <Alert className="text-green-400 border-green-400">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>
+                Your session has expired. Please log in again.
+              </AlertDescription>
+            </Alert> */}
           </div>
           <div className="col-span-2 flex gap-2">
             <Button variant="outline" className="border border-[#ea580c]">
