1601027fc4708ebb317224d65bd2d3689a951e56
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-03-06 23:17:56 -0600

    feat(api): create api for get drivers

--- app/api/driver/route.ts
+++ app/api/driver/route.ts
@@ -43,23 +43,58 @@ export async function POST(req: Request) {
         userId: user.id,
       },
     });
-    return Response.json({driver: newDriver, status: 201});
+    return Response.json({ driver: newDriver, status: 201 });
   } catch (error) {
-    return Response.json({ errorMessage: 'error creating driver', status: 500 });
+    return Response.json({
+      errorMessage: 'error creating driver',
+      status: 500,
+    });
   }
 }
 
-export async function GET() {
+export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
   const prisma = new PrismaClient();
+  const { searchParams } = new URL(req.url);
+  const page = parseInt(searchParams.get('page') || '1', 10);
+  const perPage = 10;
 
   if (!session || !session.user) {
-    return Response.json({ errorMessage: 'unauthorized', status: 401 });
+    return Response.json({ error: 'unauthorized', status: 401 });
+  }
+  if (isNaN(page)) {
+    return Response.json({
+      error: "El parámetro 'page' debe ser un valor numérico.",
+      status: 400,
+    });
+  } else if (page < 1) {
+    return Response.json({
+      error: "El parámetro 'page' deben ser números positivos.",
+      status: 400,
+    });
   }
   try {
-    const drivers = await prisma.drivers.findMany();
-    return Response.json({data: drivers, status: 200});
+    const totalDrivers = await prisma.drivers.count();
+    const totalPages = Math.ceil(totalDrivers / perPage);
+    const remainingItems = totalDrivers - (page - 1) * perPage;
+    const currentPageSize = Math.min(perPage, remainingItems);
+    const drivers = await prisma.drivers.findMany({
+      skip: (page - 1) * perPage,
+      take: currentPageSize,
+      orderBy: { createdAt: 'desc' },
+    });
+
+    if (remainingItems <= 0) {
+      return Response.json({ error: 'Página fuera de rango.', status: 404 });
+    }
+    return Response.json({
+      page,
+      per_page: currentPageSize,
+      total: totalDrivers,
+      total_pages: totalPages,
+      data: drivers,
+    });
   } catch (error) {
-    return Response.json({error, status: 500})
+    return Response.json({ error, status: 500 });
   }
 }
