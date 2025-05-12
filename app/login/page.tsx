d168866f2807461fe95aabfb81447bd59043394b
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-12 17:51:59 -0600

    feat: redirect to dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,13 @@
+import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
-import { useRouter } from "next/navigation";
 import { LoginForm } from "@/components/login-form";
 
 export default async function LoginPage() {
   const session = await getServerSession(authOptions);
-  const router = useRouter();
 
   if (session) {
-    router.push("/dashboard");
+    redirect("/dashboard");
   }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
