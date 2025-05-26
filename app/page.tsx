7a9bcf86f53766900184d5d4d16aae99cf8d91af
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-25 23:04:50 -0600

    feat: add clockIn page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { LoginHome } from "@/components/home/loginHome"
+// import { LoginHome } from "@/components/home/loginHome";
+import { ClockIn } from "@/components/home/clockIn";
 
 export default function Home() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-black p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginHome />
+        {/* <LoginHome /> */}
+        <ClockIn />
       </div>
     </div>
-  )
+  );
 }
---
+++ components/home/clockIn.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import LogoForzaDeliveryWhite from "@/public/logo-forza-delivery-white.svg";
+import LogoForzaDeliveryBlack from "@/public/logo-forza-delivery-black.svg";
+
+import { Calendar } from "lucide-react";
+import { IdCard } from "lucide-react";
+import { MapPinX } from "lucide-react";
+
+export function ClockIn() {
+  const { theme } = useTheme();
+
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5 items-center">
+        <Image
+          src={
+            theme == "light" ? LogoForzaDeliveryBlack : LogoForzaDeliveryWhite
+          }
+          alt="Logo Forza Delivery Express"
+          width={250}
+        />
+        <span className="text-xl font-semibold text-[#ea5d1d]">
+          Control Horario Recos
+        </span>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-2 h-10 px-4 py-2 rounded-md font-medium border border-input bg-background">
+          <Calendar className="w-5 h-5" />
+          <span>25/05/2025 - 20:50 PM</span>
+        </div>
+        <div className="flex items-center gap-2 h-10 px-4 py-2 rounded-md font-medium border border-input bg-background">
+          <IdCard className="w-5 h-5" />
+          <span>Denilson Yovany Morales</span>
+        </div>
+        <div className="flex items-center gap-2 h-10 px-4 py-2 rounded-md font-medium text-[#7f1d1d] border border-[#7f1d1d] bg-background">
+          <MapPinX className="w-5 h-5" />
+          <span>Ubicación no detectada</span>
+        </div>
+      </div>
+      <form className="flex flex-col gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="file">Insertar fotografía</Label>
+          <p className="text-[12px] text-muted-foreground">
+            Tome una foto del establecimiento como prueba de su ingreso
+          </p>
+          <Input id="file" type="file" required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="file">Permitir ubicación</Label>
+          <p className="text-[12px] text-muted-foreground">
+            Active la ubicación de su dispositivo para poder marcar su horario
+          </p>
+          <Button variant="secondary">Activar la ubicación</Button>
+        </div>
+        <Button type="submit" className="w-full" disabled>
+          Marcar Horario
+        </Button>
+      </form>
+    </div>
+  );
+}
