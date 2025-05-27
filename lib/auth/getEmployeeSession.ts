b00c83fbbbfb968e9580d33ad37564b69ea1c83e
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-27 17:46:43 -0600

    feat: typed helper

--- lib/auth/getEmployeeSession.ts
+++ lib/auth/getEmployeeSession.ts
@@ -3,13 +3,20 @@ import { cookies } from 'next/headers';
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export async function getEmployeeSession() {
+type EmployeeSession = {
+  uuid: string;
+  fullname: string;
+  position: string;
+  iat: number;
+  exp: number;
+};
+export async function getEmployeeSession(): Promise<EmployeeSession | null> {
   const cookieStore = await cookies();
   const token = cookieStore.get('employee-token')?.value;
 
   if (!token) return null;
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET) as EmployeeSession;
     return decoded;
   } catch (error) {
     console.log(error);
