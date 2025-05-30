6ef48687fc5898799af018cd291e5f9de44a1917
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-29 18:48:03 -0600

    feat: lib for get location

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,7 @@ import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { GetLocationType } from '@/types/clockInType';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -12,4 +13,37 @@ export const now = () => dayjs.tz();
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function getLocationUtils(): Promise<GetLocationType> {
+  if (!navigator.geolocation) {
+    return {
+      status: false,
+      message: 'El navegador no soporta la geolocalización.',
+    };
+  }
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const pos = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+
+        resolve({
+          status: true,
+          message: 'Ubicación detectada exitosamente.',
+          data: pos,
+        });
+      },
+      (error) => {
+        resolve({
+          status: false,
+          message: 'Ubicación no detectada.',
+          error,
+        });
+      }
+    );
+  });
 }
