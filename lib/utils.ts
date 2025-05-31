8d101fe8f7befd68a7e8745f2db35be36b0f3987
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-31 17:36:27 -0600

    feat: add utilities

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,10 @@ import timezone from 'dayjs/plugin/timezone';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { GetLocationType } from '@/types/clockInType';
+import imageCompression from 'browser-image-compression';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '@/lib/firebase';
+import { stateEnumType } from '@/lib/zod/clockIn';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -47,3 +51,66 @@ export async function getLocationUtils(): Promise<GetLocationType> {
     );
   });
 }
+
+export const imageCompressionUtils = async (file: File) => {
+  const options = {
+    maxSizeMB: 0.2,
+    maxWidthOrHeight: 1080,
+    useWebWorker: true,
+  };
+
+  try {
+    const compressedFile = await imageCompression(file, options);
+    return {
+      success: true,
+      image: compressedFile,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      image: file,
+      error,
+    };
+  }
+};
+
+export const uploadPhotoUtils = async (file: File, uuid: string) => {
+  try {
+    const storageRef = ref(storage, `reports/${uuid}`);
+    await uploadBytes(storageRef, file);
+
+    const downloadURL = await getDownloadURL(storageRef);
+    return {
+      success: true,
+      url: downloadURL,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error,
+    };
+  }
+};
+
+export const scheduleValidation = (
+  schedule: string,
+  currentTime: string
+): stateEnumType => {
+  const states: Record<stateEnumType, stateEnumType> = {
+    ADMIN: 'ADMIN',
+    INGRESADO: 'INGRESADO',
+    PENDIENTE: 'PENDIENTE',
+    INGRESO_TARDE: 'INGRESO_TARDE',
+  };
+  const [scheduleHour, scheduleMinute] = schedule.split(':').map(Number);
+  const [currentHour, currentMinute] = currentTime.split(':').map(Number);
+
+  const scheduleInMinutes = scheduleHour * 60 + scheduleMinute;
+  const currentInMinutes = currentHour * 60 + currentMinute;
+
+  if (currentInMinutes <= scheduleInMinutes) {
+    return states.INGRESADO;
+  } else {
+    return states.INGRESO_TARDE;
+  }
+};
