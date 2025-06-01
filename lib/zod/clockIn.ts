5c7845c690933410b79ced7979675bb331d5fc2f
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-05-31 18:25:32 -0600

    feat(zod): clock-in schema

--- lib/zod/clockIn.ts
+++ lib/zod/clockIn.ts
@@ -1,3 +1,16 @@
 import { z } from 'zod';
+import { reportStateSchema } from '@/lib/zod/report';
 
 export const uuidSchema = z.string().uuid();
+
+const stateEnum = reportStateSchema.shape.state;
+export type stateEnumType = z.infer<typeof stateEnum>;
+
+export const clockInSchema = z.object({
+  uuid: z.string().uuid().optional(),
+  employeeUUID: z.string().uuid(),
+  latitude: z.number().optional(),
+  longitude: z.number().optional(),
+  photoURL: z.string().optional(),
+});
+export type clockInSchemaType = z.infer<typeof clockInSchema>
